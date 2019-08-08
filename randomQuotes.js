3d7bb800b4162a8c14b9a3962a92e31f2e65361a
Jessica Billingslea <jessicrr@gmail.com>
2019-08-08 11:41:22 -0400

    first commit

---
+++ randomQuotes.js
@@ -0,0 +1,14 @@
+const quotes = [
+  "Random Quote 1",
+  "Random Quote 2",
+  "Random Quote 3",
+  "Random Quote 4",
+  "Random Quote 5",
+  "Random Quote 6",
+  "Random Quote 7",
+  "Random Quote 8",
+  "Random Quote 9",
+  "Random Quote 10"
+]
+
+module.exports = quotes;
---
+++ server.js
@@ -0,0 +1,12 @@
+const http = require("http");
+const ranQuotes = require("./randomQuotes.js");
+
+http.createServer((req, res) => {
+
+  var randomQuote = ranQuotes[Math.floor(Math.random()*ranQuotes.length)]
+
+  res.setHeader("Content-Type", "application/json");
+  res.writeHead(200);
+  res.write(JSON.stringify(randomQuote));
+  res.end()
+}).listen(8989);
