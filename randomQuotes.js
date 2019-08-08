839b575db81d1d02295c31ee3f1adfc9c3bf35d0
Jessica Billingslea <jessicrr@gmail.com>
2019-08-08 12:02:31 -0400

    added real quotes

--- randomQuotes.js
+++ randomQuotes.js
@@ -1,14 +1,14 @@
 const quotes = [
-  "Random Quote 1",
-  "Random Quote 2",
-  "Random Quote 3",
-  "Random Quote 4",
-  "Random Quote 5",
-  "Random Quote 6",
-  "Random Quote 7",
-  "Random Quote 8",
-  "Random Quote 9",
-  "Random Quote 10"
+  "He touched the butt. – Tad, Finding Nemo",
+  "My bark is worse than my bite. – Grandmother Willow, Pocahontas",
+  "Aw, you guys made me ink. – Pearl, Finding Nemo",
+  "Motto? Nothing, what’s a motto with you? – Timon, The Lion King",
+  "We can stay up late swapping manly stories, and in the morning..I'm making waffles. - Donkey, Shrek",
+  "One often meets his destiny on the road he takes to avoid it - Oogway, Kung Fu Panda",
+  "I never look back. darling. It distracts from the now - Edna, The Incredibles",
+  "Ladies don't start fights, but they cn finish them - Marie, The Aristocats",
+  "The flower that blooms in adversity is the most rare and beautiful of all - The Emperor, Mulan",
+  "You buttered your bread. Now sleep in it. - Jiminy Cricket, Pinocchio"
 ]
 
 module.exports = quotes;
