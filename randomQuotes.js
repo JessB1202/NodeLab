e0eaacfc18f1835dee602035a4fd3c1c55b5de08
Jessica Billingslea <jessicrr@gmail.com>
2019-08-08 12:06:26 -0400

    corrected grammer and spelling

--- randomQuotes.js
+++ randomQuotes.js
@@ -4,10 +4,10 @@ const quotes = [
   "Aw, you guys made me ink. – Pearl, Finding Nemo",
   "Motto? Nothing, what’s a motto with you? – Timon, The Lion King",
   "We can stay up late swapping manly stories, and in the morning..I'm making waffles. - Donkey, Shrek",
-  "One often meets his destiny on the road he takes to avoid it - Oogway, Kung Fu Panda",
-  "I never look back. darling. It distracts from the now - Edna, The Incredibles",
-  "Ladies don't start fights, but they cn finish them - Marie, The Aristocats",
-  "The flower that blooms in adversity is the most rare and beautiful of all - The Emperor, Mulan",
+  "One often meets his destiny on the road he takes to avoid it. - Oogway, Kung Fu Panda",
+  "I never look back, darling. It distracts from the now. - Edna, The Incredibles",
+  "Ladies don't start fights, but they can finish them. - Marie, The Aristocats",
+  "The flower that blooms in adversity is the most rare and beautiful of all. - The Emperor, Mulan",
   "You buttered your bread. Now sleep in it. - Jiminy Cricket, Pinocchio"
 ]
 
